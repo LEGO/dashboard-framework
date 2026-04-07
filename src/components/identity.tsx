e703a2d328a2e3c8a91fb6ff4befd46fb170b6ce
Lorenzo Setale <lorenzo.setale@LEGO.com>
2026-04-07 11:07:44 +0200

    refactor(auth): moves authentication around
    Signed-off-by: Lorenzo Setale <lorenzo.setale@LEGO.com>

--- src/components/identity.tsx
+++ src/components/identity.tsx
@@ -1,5 +1,21 @@
 import { useAuth } from "react-oidc-context";
+import { AuthProvider } from "react-oidc-context";
+import { PrimeReactProvider } from "primereact/api";
 
+
+const oidcConfig = {
+  authority: "https://login.microsoftonline.com/1d063515-6cad-4195-9486-ea65df456faa",
+  client_id: "3e0642f0-e0de-4a20-bf67-1f66d39863c0",
+  redirect_uri: window.location.origin,
+  response_type: "code",
+  scope: "openid profile email",
+  onSigninCallback: (_user: User | void): void => {
+    window.history.replaceState({}, document.title, window.location.pathname);
+  },
+};
+
+
+// Provides identity features to other components
 export default function IdentityComponent() {
   const auth = useAuth();
 
@@ -38,4 +54,17 @@ export default function IdentityComponent() {
       <button onClick={() => void auth.signinRedirect()}>Log in</button>
     </div>
   );
+ }
+
+// Wrapper to add authentication to The LEGO Group SSO
+export function LEGOAuthProvider({ children }) {
+  return (
+    <AuthProvider {...oidcConfig}>
+      <PrimeReactProvider unstyled={true}>
+        <div className="app">
+          { children }
+        </div>
+      </PrimeReactProvider>
+    </AuthProvider>
+  )
 }
--- src/frontend.tsx
+++ src/frontend.tsx
@@ -1,36 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { DashboardGenerator } from "./Wizard.tsx";
-import { AuthProvider } from "react-oidc-context";
-import { PrimeReactProvider, PrimeReactContext } from "primereact/api";
-
+import { LEGOAuthProvider } from "./components/identity.tsx"
 // import "primereact/resources/themes/lara-light-cyan/theme.css";
 
 import "./index.css";
 
-const oidcConfig = {
-  authority:
-    "https://login.microsoftonline.com/1d063515-6cad-4195-9486-ea65df456faa",
-  client_id: "3e0642f0-e0de-4a20-bf67-1f66d39863c0",
-  redirect_uri: window.location.origin,
-  response_type: "code",
-  scope: "openid profile email",
-  onSigninCallback: (_user: User | void): void => {
-    console.log("User signed in:", _user);
-    window.history.replaceState({}, document.title, window.location.pathname);
-  },
-};
-
 const elem = document.getElementById("root")!;
 const app = (
   <StrictMode>
-    <AuthProvider {...oidcConfig}>
-      <PrimeReactProvider unstyled={true}>
-        <div className="app">
-          <DashboardGenerator />
-        </div>
-      </PrimeReactProvider>
-    </AuthProvider>
+    <LEGOAuthProvider>
+      <div className="app">
+        <DashboardGenerator />
+      </div>
+    </LEGOAuthProvider>
   </StrictMode>
 );
 
